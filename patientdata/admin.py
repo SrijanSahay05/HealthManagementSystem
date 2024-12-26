3128aa06aff500f97d20ec5dc6f6760d65733d59
Srijan Sahay <srijan05sahay@gmail.com>
2024-12-26 22:54:00 +0530

    added patient profile and made medicine timetable model

--- patientdata/admin.py
+++ patientdata/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Days, PatientMedicineSchedule
 
-# Register your models here.
+class DaysInline(admin.TabularInline):
+    model = PatientMedicineSchedule.medicine_days.through
+    extra = 1
+
+class PatientMedicineScheduleAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'medicine_name', 'medicine_dosage', 'medicine_frequency', 'medicine_time', 'medicine_start_date', 'medicine_end_date')
+    search_fields = ('patient__user__email', 'medicine_name')
+    list_filter = ('medicine_days', 'medicine_start_date', 'medicine_end_date')
+    # inlines = [DaysInline]
+
+admin.site.register(Days)
+admin.site.register(PatientMedicineSchedule, PatientMedicineScheduleAdmin)
